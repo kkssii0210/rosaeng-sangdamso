7271a4d9341b10ceffac725870edcbf9a2b8d43c
kkssii0210 <kkssii0210@naver.com>
2026-05-18 21:58:54 +0900

    feat: scaffold spring boot backend

---
+++ backend/src/main/java/com/rosaeng/sangdamso/RosaengSangdamsoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rosaeng.sangdamso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RosaengSangdamsoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RosaengSangdamsoBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/rosaeng/sangdamso/RosaengSangdamsoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rosaeng.sangdamso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RosaengSangdamsoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
